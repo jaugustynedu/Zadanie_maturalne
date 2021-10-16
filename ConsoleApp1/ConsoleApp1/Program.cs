81d1c70114214d9bbe266eb78b66308b2e4bc756
Jakub Augustyn <Jakub.Augustyn@microsoft.wsei.edu.pl>
2021-10-16 10:02:03 +0200

    Check random number

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+            int random = r.Next(0, 10);
+            Console.WriteLine("Wybierz liczbe od 0 do 10: ");
+            int x = Int32.Parse(Console.ReadLine());
+
+            if (x == random){
+                Console.WriteLine("Zgadłeś, brawo!");
+            }
+            else {
+                Console.WriteLine("Nie udało się. Wylosowana liczba to: "+random);
+            }
+
+
+
+
+        }
+    }
+}
