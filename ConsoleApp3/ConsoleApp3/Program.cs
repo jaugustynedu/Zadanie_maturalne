9e75eca301629ce8a54e43a320e947d486720158
Jakub Augustyn <Jakub.Augustyn@microsoft.wsei.edu.pl>
2021-11-13 10:53:39 +0100

    Zadanie 4.1, Dokumenty - Matura

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+
+            switch (x)
+            {
+                case > 5:
+                    Console.WriteLine("Liczba większa od 5");
+                    break;
+                case < 5:
+                    Console.WriteLine("Liczba mniejsza od 5");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+        }
+    }
+}
